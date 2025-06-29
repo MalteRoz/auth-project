cc99387edb794678285c1a9384f10047c44fe4a1
Malte Rozenbeek <malte.rozenbeek@gmail.com>
2025-06-29 12:39:57 +0200

    ♻️

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,12 @@
-console.log("HEJ");
+import express, { json } from "express";
+
+const app = express();
+
+app.use(json());
+
+console.log("hello world");
+
+const port = process.env.PORT || 3000;
+app.listen(port, async () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
