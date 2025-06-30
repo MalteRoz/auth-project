934af6d8a48737917c52d2086f6ccb6b5c451295
Malte Rozenbeek <malte.rozenbeek@gmail.com>
2025-06-30 16:10:42 +0200

    initiated docker to containerize both frontend and backend.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,9 +4,11 @@ const app = express();
 
 app.use(json());
 
-console.log("hello world");
+app.get("/", function (req, res) {
+  res.send("Testus");
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, async () => {
+app.listen(Number(port), "0.0.0.0", async () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
